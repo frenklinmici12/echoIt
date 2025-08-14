923d8d74d8ec0218d16170ecb7a25b68dc486d81
frenklinmici12 <frenklinmici12@gmail.com>
2025-08-13 22:44:15 -0500

    test

--- echoIt.go
+++ echoIt.go
@@ -15,11 +15,14 @@ import (
 func main() {
 	var input kernelTypes.String // will store input in here.
 
-	status := altEthos.ReadStream(syscall.Stdin, &input)
+	status := altEthos.ReadStream(syscall.Stdin, &input) // Read from stdin (0), put that into the variable 'input' ReadStream returns status.
+	
+	// if something goes wrong (not StatusOk), exit
 	if status != syscall.StatusOk {
 		log.Println("Error reading from stdin. Exiting.")
 		altEthos.Exit(status)
 	}
 
+	//echo the input back to the user
 	fmt.Println("You entered: ", input)
 }
