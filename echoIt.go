b5e6c8ec317dd2d57e0d0994eaf90a1713a14a54
frenklinmici12 <frenklinmici12@gmail.com>
2025-08-13 22:25:33 -0500

    msg

---
+++ echoIt.go
@@ -0,0 +1,15 @@
+// Author: Frenklin Mici 08/13/25
+// Purpose: Demo for taking input and outputting text from stdin and stdout
+
+package main
+
+import (
+	"ethos/fmt" // for outputting
+	"ethos/altEthos" 
+	
+	"log"
+)
+
+func main() {
+	
+}
